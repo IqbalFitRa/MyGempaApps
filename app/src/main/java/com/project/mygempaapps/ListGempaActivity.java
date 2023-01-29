f7cee080900980cafc81837a2f2450dced8178eb
IqbalFitRa <89772053+IqbalFitRa@users.noreply.github.com>
2023-01-29 15:29:00 +0700

    com 1

---
+++ app/src/main/java/com/project/mygempaapps/ListGempaActivity.java
@@ -0,0 +1,13 @@
+package com.project.mygempaapps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListGempaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_gempa);
+    }
+}
